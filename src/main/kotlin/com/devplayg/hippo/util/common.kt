633ecb19e7b2519b8568579ef22c14d6da8b0b10
Devplayg <devplayg@gmail.com>
2020-11-14 00:02:34 +0900

    Update common.kt

--- src/main/kotlin/com/devplayg/hippo/util/common.kt
+++ src/main/kotlin/com/devplayg/hippo/util/common.kt
@@ -6,10 +6,39 @@ import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
 
 
-fun currentMember(): CustomUserDetails? {
-    val auth: Authentication = SecurityContextHolder.getContext().authentication
-    if (auth is AnonymousAuthenticationToken) {
-        return null
+/**
+ * 현재 Username 조회
+ */
+fun currentUsername(): String {
+    val auth = SecurityContextHolder.getContext().authentication
+    return if (auth is AnonymousAuthenticationToken) {
+        ""
+    } else {
+        auth.name
     }
-    return auth.principal as CustomUserDetails
+}
+
+
+/**
+ * 현재 Username & Roles value 조회
+ */
+fun currentUsernameAndRoles(): Pair<String, Int> {
+    val auth = SecurityContextHolder.getContext().authentication
+    return if (auth is AnonymousAuthenticationToken) {
+        Pair("", 0)
+    } else {
+        Pair(auth.name!!, authoritiesToValue(auth.authorities))
+    }
+}
+
+
+/**
+ * 문자열을 SortOrder로 변환
+ */
+fun toSortOrder(order: String): SortOrder {
+    return if (order.trim().toLowerCase() == "asc") {
+        SortOrder.ASC
+    } else {
+        SortOrder.DESC
+    }
 }
