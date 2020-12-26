b001299d16e7aeca3650db38d7d3d164e6613351
Devplayg <devplayg@gmail.com>
2020-12-26 12:01:46 +0900

    Update Date.kt

--- src/main/kotlin/com/devplayg/hippo/define/Date.kt
+++ src/main/kotlin/com/devplayg/hippo/define/Date.kt
@@ -1,5 +1,17 @@
 package com.devplayg.hippo.define
 
-val RFC3339Format: org.joda.time.format.DateTimeFormatter = org.joda.time.format.DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ssZ")
-//val YYYYMMDDHHIISS: org.joda.time.format.DateTimeFormatter = org.joda.time.format.DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss")
+const val YYYYMMDD = "yyyy-MM-dd"
+const val YYYYMMDDHHMMSS ="yyyy-MM-dd HH:mm:ss"
+const val YYYYMMDDHHMMSSZ ="yyyy-MM-dd HH:mm:ssZ"
+const val YYYYMMDDHHMMSSZZ = "yyyy-MM-dd'T'HH:mm:ssZZ"
+const val YYYYMMDDTHHMMSSSSSZ = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+const val HHMM = "HH:mm"
+
+
+val RFC3339Format: org.joda.time.format.DateTimeFormatter = org.joda.time.format.DateTimeFormat.forPattern(YYYYMMDDHHMMSSZ)
+val RFC3339PatternWithMilli: org.joda.time.format.DateTimeFormatter = org.joda.time.format.DateTimeFormat.forPattern(YYYYMMDDTHHMMSSSSSZ)
+val RFC3339Pattern: org.joda.time.format.DateTimeFormatter = org.joda.time.format.DateTimeFormat.forPattern(YYYYMMDDHHMMSSZZ)
+val DefaultDateTimeFormat = DateTimeFormat.forPattern(YYYYMMDDHHMMSS)!!
+val DefaultDateFormat = DateTimeFormat.forPattern(YYYYMMDD)!!
+val DefaultTimeFormat = DateTimeFormat.forPattern("HH:mm")!!
 
