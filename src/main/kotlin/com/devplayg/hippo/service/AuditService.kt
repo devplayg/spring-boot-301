b8785b30e051ea0231a91a2b90bc0b19ce54e7ea
Devplayg <devplayg@gmail.com>
2020-12-27 01:43:17 +0900

    Update AuditService.kt

--- src/main/kotlin/com/devplayg/hippo/service/AuditService.kt
+++ src/main/kotlin/com/devplayg/hippo/service/AuditService.kt
@@ -9,81 +9,37 @@ import org.springframework.stereotype.Service
 
 @Service
 class AuditService(
-        private val auditRepo: AuditRepo
+        private val auditRepo: AuditRepo,
+        private val memberCacheRepo: MemberCacheRepo
 ) {
-    fun getAudits(filter: AuditFilter) = transaction {
-        auditRepo.find(filter).map {
-            toAuditDto(it)
-        }
+    /**
+     * 조회 (Fast paging)
+     */
+    fun find(filter: AuditFilter) = auditRepo.find(filter).map{
+        it.member = memberCacheRepo.findById(it.memberId)
+        return@map it
     }
 
-    fun getAuditsWithTotal(filter: AuditFilter) = transaction {
-        PageData(getAudits(filter), auditRepo.predicate(filter).count())
+
+    /**
+     * 조회 (Normal paging)
+     */
+    fun findWithTotal(filter: AuditFilter) = auditRepo.findWithTotal(filter)
+
+
+    /**
+     * 보안감사 기록
+     */
+    fun audit(category: Int, message: Any? = null) {
+        _audit(currentUsername(), category, message)
+    }
+    fun _audit(username: String, category: Int, message: Any? = null) {
+        // wondory: 캐시가 사라졌을 경우, 추후 대비 필요
+        val memberDto = memberCacheRepo.findByUsername(username)
+        auditLog(memberDto?.id ?: 0, category, message)
+    }
+
+    fun auditBySystem(category: Int, message: Any? = null) {
+        auditLog(SystemMemberId, category, message)
     }
 }
-
-//    companion object {
-//        fun write(ip: Long, category: Int, message: String) = transaction {
-//            Audit.new {
-//                this.message = message
-//                this.ip = ip
-//                this.category = category
-//            }
-//        }
-//    }
-
-//    fun find2(filter: AuditFilter) = transaction{
-//        val list :List<AuditDto> = auditRepo.find(filter).map {
-//            mapToAuditDto(it)
-//        }
-//        val total: Long = auditRepo.find(filter).count()
-//        PageData(list, total)
-//    }
-
-//
-//    fun findAll(filter: AuditFilter) = transaction {
-//        var q: Query = auditRepo.findAll(filter)
-//        val list: List<AuditDto> = q.map {
-//            mapToAuditDto(it)
-//        }
-//        val total: Long = q.count()
-//        val total2: Long = q.count()
-//
-//
-////        q.map
-////        auditRepo.findAll(filter).map {
-////            mapToAuditDto(it)
-////        }
-//    }
-
-//    fun find(filter :AuditFilter) = transaction {
-//        auditRepo.find(filter)
-//        Audit.find {  }.limit(filter.size, offset )
-//        Audits.select {
-//        }.offset(10, 10)
-//    }
-//    fun find(filter: AuditFilter) = transaction {
-////        var list : List<Audit> = auditRepo.find(filter).map {  }
-//        auditRepo.find(filter).map {
-//            mapToAuditDto(it)
-//        }
-//    }
-
-//    fun find(filter: AuditFilter) = transaction {
-////        var list : List<Audit> = auditRepo.find(filter).map {  }
-//        auditRepo.find(filter).map {
-//            mapToAuditDto(it)
-//        }
-//    }
-//    }
-
-
-//
-//fun getMatchedColumn(cols: List<Column<*>>, colName: String): Column<*>? {
-//    for (it in cols) {
-//        if (colName == it.name) {
-//            return it
-//        }
-//    }
-//    return null
-//}
