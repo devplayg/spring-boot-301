b7310c438b98aa98f1eea905e943a894f270ebab
Devplayg <devplayg@gmail.com>
2020-12-26 11:58:31 +0900

    Update MemberRole.kt

--- src/main/kotlin/com/devplayg/hippo/define/MemberRole.kt
+++ src/main/kotlin/com/devplayg/hippo/define/MemberRole.kt
@@ -1,9 +1,50 @@
 package com.devplayg.hippo.define
 
-enum class MemberRole(var description: String, var value: Int) {
-        Admin("member.level.administrator", 1 shl (10)), // 1024
-        Sheriff("member.level.sheriff", 1 shl (9)), // 512
-        User("member.level.user", 1 shl (8)) // 256
+const val MemberRolePrefix = "ROLE_"
+enum class MemberRole(var description: String, var value: Int) : GrantedAuthority {
+    Admin("member.level.administrator", 1 shl (10)), // 1024
+    Sheriff("member.level.sheriff", 1 shl (8)), // 256
+    User("member.level.ads-monitor", 1 shl (0)); // 1
+
+    override fun getAuthority(): String {
+        return name
+    }
 }
 
-const val SystemMemberId = 0L
+fun getDefensiveMemberRoles(): Int {
+    var roles = 0
+    MemberRole.values().filter{
+        it.value != MemberRole.Admin.value
+    }.forEach{
+        roles = (roles or it.value)
+    }
+    return roles
+}
+
+fun authoritiesToValue(authorities: Collection<GrantedAuthority>): Int {
+    var authRoleValue = 0
+    authorities.forEach {
+        val roleName = it.authority.removePrefix(MemberRolePrefix)
+
+        try {
+            val role = MemberRole.valueOf(roleName).value
+            authRoleValue = authRoleValue or role
+        } catch(e: IllegalArgumentException) {
+        }
+    }
+    return authRoleValue
+}
+
+fun NewUsernamePasswordAuthenticationToken(auth: Authentication, roles: Int): Authentication {
+    return UsernamePasswordAuthenticationToken(
+            auth.principal,
+            null,
+            AuthorityUtils.commaSeparatedStringToAuthorityList(
+                    MemberRole.values()
+                            .filter { r -> r.value and roles > 0 }
+                            .map {
+                                MemberRolePrefix + it.name
+                            }.joinToString(",")
+            )
+    )
+}
