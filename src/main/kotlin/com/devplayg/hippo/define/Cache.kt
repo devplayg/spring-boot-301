b58a0c32911079685de66cae464d9f38571eb409
Devplayg <devplayg@gmail.com>
2020-12-26 21:29:16 +0900

    Update Cache.kt

--- src/main/kotlin/com/devplayg/hippo/define/Cache.kt
+++ src/main/kotlin/com/devplayg/hippo/define/Cache.kt
@@ -9,3 +9,10 @@ const val CacheMemberPrefix = "mbr-uname:"
 const val CacheMemberIdPrefix = "mbr-id:"
 const val CacheMemberOnline = "mbr-online:"
 const val CacheMember2FA = "mbr-2fa:"
+
+/**
+ * Spring-boot Redis
+ */
+const val CacheSpringBootRedisMemberPrefix = "spring:session:index:org.springframework.session.FindByIndexNameSessionRepository.PRINCIPAL_NAME_INDEX_NAME:"
+const val CacheSpringBootRedisSession = "spring:session:sessions:"
+const val CacheSpringBootRedisSessionExpire = "spring:session:sessions:expires:"
